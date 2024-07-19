d581c82ab5f1f1292d0df2b12f0a4564937603f5
Jeffery <Jeffery_jian@harmonation.com.tw>
2024-07-19 21:50:58 +0800

    first commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-primary">
+    <div class="container-fluid container-md px-0 py-md-3 d-flex align-items-center justify-content-between flex-wrap">
+      <h1 class="mb-0 logo w-100 w-md-auto py-3 py-md-0 ps-3 ps-md-0">
+        <router-link>
+          <img class="img-fluid" width="80" height="80" src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/logo-white.png?raw=true" alt="glass">
+        </router-link>
+      </h1>
+      <ul class="list-unstyled d-flex mb-0 flex-wrap justify-content-between w-100 w-md-auto">
+        <li class="w-50 w-md-auto border border-start-0 border-md-0 py-3 py-md-0 "><a class="d-block text-center mb-0 h5 text-white lh-base text-decoration-none">系列鏡框</a></li>
+        <li class="w-50 w-md-auto border border-end-0 border-md-0 py-3 py-md-0 ms-md-10"><a class="d-block text-center mb-0 h5 text-white lh-base text-decoration-none">門市據點</a></li>
+        <li class="w-50 w-md-auto border border-start-0 border-md-0 py-3 py-md-0 ms-md-10"><a class="d-block text-center mb-0 h5 text-white lh-base text-decoration-none">部落格</a></li>
+        <li class="w-50 w-md-auto border border-end-0 border-md-0 py-3 py-md-0 ms-md-10"><a class="d-block text-center mb-0 h5 text-white lh-base text-decoration-none">常見問題</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,93 @@
+<script setup>
+  import Nav from "@/components/Nav.vue";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <Nav></Nav>
+    </nav>
+  </header>
+  <div class="banner">
+    <div class="container h-100">
+      <div class="row h-100">
+        <div class="col h-100 px-0">
+          <div class="h-100 d-flex align-items-end flex-column justify-content-center pe-3 pe-lg-0">
+            <h2 class="text-secondary fs-6 fs-md-2">Promise-Desert 2020 早春系列</h2>
+            <p class="banner-text fw-bold text-secondary ">看得清，才能看得遠</p>
+            <button class="btn btn-primary fs-5">立即購買</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <main>
+    <div class="container">
+      <h3 class="text-primary">用專業的心，做專業的事</h3>
+      <div class="row">
+        <div class="col-md-6 col-xxl-3">
+          <div>
+            <div>
+              <img src="" alt="">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-xxl-3">
+          <div>
+            <div>
+              <img src="" alt="">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-xxl-3">
+          <div>
+            <div>
+              <img src="" alt="">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-xxl-3">
+          <div>
+            <div>
+              <img src="" alt="">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  .banner{
+    background: 
+      url("https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-header.png?raw=true")
+      no-repeat
+      center center / cover;
+    height: 787px;  
+  }
+  .banner-text{
+    font-size: 4rem;
+  }
+  @media (max-width: 767px) {
+    .banner{
+      background: 
+        url("https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-header-sm.png?raw=true")
+        no-repeat
+        center center / cover;
+      height: 396px;
+    }
+    .banner-text{
+      font-size: 1.5rem;
+    }
+  }
+</style>
+
